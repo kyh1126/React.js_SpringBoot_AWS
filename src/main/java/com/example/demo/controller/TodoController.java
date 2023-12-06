eef99c0437b65a4b186d598192805ce269115fd1
kyh1126 <kyh901126@nate.com>
2023-12-07 00:05:25 +0900

    [TIL/book] React.js, 스프링 부트, AWS로 배우는 웹 개발 101 (2-2-4. 컨트롤러 레이어 : 스프링 REST API 컨트롤러)

---
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+    @GetMapping
+    public String testTodo() {
+        return "Hello Todo!";
+    }
+}
