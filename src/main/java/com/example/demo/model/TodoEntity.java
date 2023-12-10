ad3df3ba39c5187993ba90b091a0191ddfc656cc
kyh1126 <kyh901126@nate.com>
2023-12-10 20:30:56 +0900

    [TIL/book] React.js, 스프링 부트, AWS로 배우는 웹 개발 101 (2-2. 백엔드 서비스 아키텍처)

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,15 +1,25 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "Todo")
 public class TodoEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id;     // 이 오브젝트의 아이디
     private String userId; // 이 오브젝트를 생성한 유저의 아이디
     private String title;  // 타이틀 예) 운동하기
